a418555069356cea7b846cf7716bed1a29b4e958
Jacob Ambroz <143131645+sixtyoneeighty@users.noreply.github.com>
2024-12-20 07:15:30 -0600

    j

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -10,7 +10,7 @@ export default async function ChatPage() {
   const session = await auth();
 
   if (!session?.user) {
-    redirect('/sign-in');
+    redirect('/login');
   }
 
   const id = generateUUID();
